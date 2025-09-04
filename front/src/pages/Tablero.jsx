5d793c330c589a0151eb3ef30dbab1c2548248b4
rubencala30 <rubencala30@gmail.com>
2025-09-04 13:44:06 +0200

    Preparación tablero

--- front/src/pages/Tablero.jsx
+++ front/src/pages/Tablero.jsx
@@ -1,11 +1,14 @@
-import "../styles/tablero.css"
+import "../styles/tablero.css";
+import BaseLayout from "../baseLayout/BaseLayout";
 
 const Tablero = () => {
-    return (
-        <header>
-            <h2>hola mundito</h2>
-        </header>
-    );
+  return (
+    <BaseLayout>
+      <div class="h-dvh flex items-center justify-center bg-gradient-to-br from-[#CD5C1D] via-[#a84310] to-[#5c1d0a] p-6">
+        <p>Tablero</p>
+      </div>
+    </BaseLayout>
+  );
 };
 
 export default Tablero;
