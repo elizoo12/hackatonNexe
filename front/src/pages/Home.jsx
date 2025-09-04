c61b57981cf298cb6299d37b54a22f2bd8147a1f
Rene <poencima22@gmail.com>
2025-09-04 11:08:35 +0200

    layout incoming

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import BaseLayout from "../baseLayout/BaseLayout";
+
+
 const Home = () => {
   return (
+    <BaseLayout>
+
     <div class="bg-gradient-to-br from-gray-950 via-zinc-400 to to-gray-50 h-dvh">
       <h1 class="text-3xl font-bold">Home</h1>
     </div>
+    
+    </BaseLayout>
   );
 };
 
