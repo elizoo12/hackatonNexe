a4242ac724d06a140297a9dd14c6051c4dddf94c
rubencala30 <rubencala30@gmail.com>
2025-09-04 10:15:34 +0200

    diseño inicial

---
+++ front/src/Router.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div class="bg-gradient-to-br from-gray-950 via-zinc-400 to to-gray-50 h-dvh">
+      <h1 class="text-3xl font-bold">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
