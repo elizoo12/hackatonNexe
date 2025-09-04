b6c9ff3d0a0bfd4ad517e3d8e9aa39c3b95dc2e4
rubencala30 <rubencala30@gmail.com>
2025-09-04 14:51:05 +0200

    Layout

--- front/src/pages/Medico.jsx
+++ front/src/pages/Medico.jsx
@@ -7,7 +7,7 @@ const Medico = () => {
       <div className="h-dvh flex flex-col items-center bg-gradient-to-br from-[#CD5C1D] via-[#a84310] to-[#5c1d0a] p-6 gap-8">
         <div className="w-full max-w-7xl bg-white/90 backdrop-blur-md rounded-2xl shadow-2xl p-6">
           <h1 className="text-4xl font-bold text-center text-gray-800 mb-6">
-            Listado de Pacientes
+            Listado de Usuarios
           </h1>
           <div className="flex flex-col gap-4">
             <div className="flex items-center justify-between bg-gray-100 rounded-xl p-4 shadow hover:shadow-lg transition">
@@ -17,7 +17,7 @@ const Medico = () => {
                   className="w-16 h-16 rounded-full object-cover"
                 />
                 <h2 className="text-xl font-semibold text-gray-800">
-                  Nombre del Paciente
+                  Nombre del Usuario
                 </h2>
               </div>
               <button className="bg-gradient-to-r from-[#FF7F50] to-[#FF4500] text-white px-4 py-2 rounded-full font-semibold border-2 border-[#a84310] hover:from-[#FF6347] hover:to-[#FF8C00] transition">
@@ -29,11 +29,10 @@ const Medico = () => {
               <div className="flex items-center gap-4">
                 <img
                   src={descargaImg}
-                  alt="Paciente"
                   className="w-16 h-16 rounded-full object-cover"
                 />
                 <h2 className="text-xl font-semibold text-gray-800">
-                  Nombre del Paciente 2
+                  Nombre del Usuario
                 </h2>
               </div>
               <button className="bg-gradient-to-r from-[#FF7F50] to-[#FF4500] text-white px-4 py-2 rounded-full font-semibold border-2 border-[#a84310] hover:from-[#FF6347] hover:to-[#FF8C00] transition">
